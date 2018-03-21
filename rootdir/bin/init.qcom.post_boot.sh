5f99015e4a7772d6201b3ddcb01557063e96bc55
Tim Murray <timmurray@google.com>
2018-03-21 11:13:57 -0700

    msm8998-common: rootdir: disable mem_latency governor
    Unclear if advantages are worth cumulative power draw.
    
    bug 73364974
    Test: boot taimen, memlat is powersave
    
    Change-Id: I7b5eb176d0f22281e687b9577f5eedf311fb4a11

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -46,12 +46,6 @@ do
     echo 1600 > $cpubw/bw_hwmon/idle_mbps
 done
 
-for memlat in /sys/class/devfreq/*qcom,memlat-cpu*
-do
-    echo "mem_latency" > $memlat/governor
-    echo 10 > $memlat/polling_interval
-    echo 400 > $memlat/mem_latency/ratio_ceil
-done
 echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
 
 setprop sys.post_boot.parsed 1
