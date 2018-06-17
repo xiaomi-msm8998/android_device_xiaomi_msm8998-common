7ed3b0080ddc26aa791b43f9032eda39684cbb8f
Vol Zhdanov <wight554@gmail.com>
2018-06-17 16:12:15 +0000

    msm8998-common: import powerhint

--- msm8998.mk
+++ msm8998.mk
@@ -307,6 +307,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service.xiaomi_msm8998
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Properties
 include $(LOCAL_PATH)/vendor_prop.mk
 
