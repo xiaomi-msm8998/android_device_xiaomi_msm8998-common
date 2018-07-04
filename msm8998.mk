34248b5165e3a81f1eab87af7dd4b769a4432564
Vol <wight554@gmail.com>
2018-07-04 22:31:11 +0000

    msm8998-common: drop doze
    * no way for chiron's proximity

--- msm8998.mk
+++ msm8998.mk
@@ -167,10 +167,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
