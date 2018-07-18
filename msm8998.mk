2c5af3f7244e243a6f0fca1be1b77f5121a327b2
Demon Singur <demonsingur@gmail.com>
2018-07-18 18:22:32 +0000

    msm8998-common: shim missing skia symbols
    Change-Id: I79559c3c4e692de0819b64975e6d339b4c9969fc

--- msm8998.mk
+++ msm8998.mk
@@ -128,6 +128,9 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
+    libcamshim
+
+PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
