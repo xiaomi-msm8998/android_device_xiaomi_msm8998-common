a0280f15b674eb9832c347ccb21ed235e457853b
Demon Singur <demonsingur@gmail.com>
2018-08-02 13:44:19 +0000

    msm8998-common: switch to QCOM HIDL bluetooth stack
    Change-Id: I264a39fc889b02632ac070cf24fde665e19adeec

--- msm8998.mk
+++ msm8998.mk
@@ -116,12 +116,7 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    AntHalService
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -138,9 +133,6 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor
-
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
