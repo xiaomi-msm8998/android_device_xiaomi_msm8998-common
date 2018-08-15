10d1068866b12bbff0ace307c6c66d3d657a61c7
LMMFHHO <39061882+LMMFHHO@users.noreply.github.com>
2018-08-15 16:45:07 +0200

    comment enforce RRO

--- msm8998.mk
+++ msm8998.mk
@@ -21,8 +21,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+#PRODUCT_ENFORCE_RRO_TARGETS := \
+#    framework-res
 
 # Permissions
 PRODUCT_COPY_FILES += \
