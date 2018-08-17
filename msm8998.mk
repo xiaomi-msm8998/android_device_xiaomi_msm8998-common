f1ce6f0604e585d6450a2cfa95a23b281c0a1454
Vol Zhdanov <wight554@gmail.com>
2018-08-17 09:56:04 +0000

    Revert "msm8998-common: Build with VNDK=current"
    This reverts commit d96c7ac55ac8e1d08a49a2423cd42b6a4e499bd0.

--- msm8998.mk
+++ msm8998.mk
@@ -335,9 +335,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service
 
 # VNDK
-PRODUCT_PACKAGES += \
-    vndk_package
-
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:vendor/lib64/android.hardware.gnss@1.0-v27.so
 
