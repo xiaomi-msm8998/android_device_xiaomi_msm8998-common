554638d79fca3a13a07be329f5f47797b88f8658
Vol Zhdanov <wight554@gmail.com>
2018-08-17 09:44:42 +0000

    msm8998-common: use prebuilt shim for camera

--- msm8998.mk
+++ msm8998.mk
@@ -123,9 +123,6 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    libcamshim.vendor
-
-PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
