421e7329f483f08f116ba04b0a8ffce27d7998ad
Shashi Shekar Shankar <ssbang@codeaurora.org>
2019-01-17 16:41:29 +0530

    msm8998-common: perf: Move properties, update perf-hal
    1. Move properties to vendor config store.
    2. Move perf-hal to 2.0
    
    Change-Id: Ie9191ed2f9eea38a834f97950c244e363f5abaa5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -150,8 +150,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.qti.core_ctl_max_cpu=4 \
-    ro.vendor.qti.core_ctl_min_cpu=0 \
-    ro.vendor.qti.sys.fw.bg_apps_limit=60
+    ro.vendor.qti.core_ctl_min_cpu=0
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
