ad50423f4e4c4061f518fbb29495bed3fa1cfd06
Arian <arian.kulmer@web.de>
2019-11-21 19:50:16 +0100

    msm8998-common: Update DPM from LA.UM.8.2.r1-05700-sdm660.0
    Change-Id: I1b5a50acba713d30dc8e341a68527c88de072e9e

--- extract-files.sh
+++ extract-files.sh
@@ -59,11 +59,11 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-    lib64/libdpmframework.so)
-        patchelf --add-needed "libshim_dpmframework.so" "${2}"
-        ;;
     lib64/libwfdnative.so)
         patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
+        ;;
+    product/lib64/libdpmframework.so)
+        patchelf --add-needed "libshim_dpmframework.so" "${2}"
         ;;
     vendor/etc/permissions/qti_libpermissions.xml)
         sed -i 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"
