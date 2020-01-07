1d2cb9f8e0037f6614a0c5b90bdedf9b36395286
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2020-01-08 00:06:25 +0100

    msm8998-common: Build ParanoidDoze
    Change-Id: I15aa87442124d30d6f69eb8d2c10ce47d26475d5

--- msm8998.mk
+++ msm8998.mk
@@ -174,10 +174,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
@@ -283,6 +279,10 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
+
+# ParanoidDoze
+PRODUCT_PACKAGES += \
+    ParanoidDoze
 
 # Power
 PRODUCT_PACKAGES += \
