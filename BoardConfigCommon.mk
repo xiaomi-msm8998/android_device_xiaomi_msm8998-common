de3f204c64890aef9e1506756d1d5eb91575316c
Arian <arian.kulmer@web.de>
2020-01-16 15:49:03 +0100

    msm8998-common: Build qti 1.2 power HAL
    Change-Id: I0780501ecf5d3a5e66036945aa64770ee49605e6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,6 +127,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_USES_NON_LEGACY_POWERHAL := true
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
