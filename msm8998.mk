9336cc0f7533e7064e6b95ce5592e89819e78223
Arian <arian.kulmer@web.de>
2020-01-16 19:39:52 +0100

    msm8998-common: Do not explicitly build Snap
    Change-Id: Ib0712b01606e9bab0ee6625c1929928bd0316f3d

--- msm8998.mk
+++ msm8998.mk
@@ -148,9 +148,6 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
-
-PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
