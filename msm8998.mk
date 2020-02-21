3f2a6ee1587f5ebfb3faaf6524022164a2d890f9
Arian <arian.kulmer@web.de>
2020-02-21 14:59:18 +0100

    msm8998-common: Add qti bluetooth audio blobs
    Change-Id: I7a2b4078ea3b1ddf78c426be64d340c8ec9d9b01

--- msm8998.mk
+++ msm8998.mk
@@ -152,9 +152,11 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    libbthost_if \
+    liba2dpoffload \
+    libbthost_if
 
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Bluetooth aptX(HD)
