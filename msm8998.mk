aa9b9e29f95d73f7477aa4f69fe1ac8030b61815
Arian <arian.kulmer@web.de>
2020-03-09 21:14:48 +0100

    Revert "msm8998-common: import media seccomp policy from MIUI 8.2.1"
    * This is available form device/qcom/common already
    
    This reverts commit c92d0505f4148079f4315afe271e7f0dc166978f.

--- msm8998.mk
+++ msm8998.mk
@@ -341,11 +341,6 @@ PRODUCT_PACKAGES += \
     libril-wrapper \
     librmnetctl
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
