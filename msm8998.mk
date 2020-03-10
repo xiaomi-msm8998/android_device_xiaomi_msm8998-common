a64b0fdcad685f075b982dfc3662844ecc3c7934
Arian <arian.kulmer@web.de>
2020-03-10 20:41:46 +0100

    msm8998-common: Drop power HAL from device tree
    Change-Id: I47a00f40e7d43a9daa51add44cb456c7d4b82518

--- msm8998.mk
+++ msm8998.mk
@@ -290,10 +290,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ParanoidDoze
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
-
 # Properties
 include $(LOCAL_PATH)/system_prop.mk
 include $(LOCAL_PATH)/vendor_prop.mk
