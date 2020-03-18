2f375df57970884165d7433b535a98bc648d2789
TheStrix <parthbhatia98@gmail.com>
2020-03-19 02:38:50 +0530

    msm8998-common: Use new QTI components from qcom/common
    Change-Id: I256f05243f614154cbdcc3ff654d3e016e8ca08b

--- msm8998.mk
+++ msm8998.mk
@@ -151,9 +151,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
-# Bluetooth aptX(HD)
-include vendor/qcom/common/bt/qti-bt.mk
-
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
@@ -305,6 +302,9 @@ PRODUCT_COPY_FILES += \
 # QMI
 PRODUCT_PACKAGES += \
     libjson
+
+# QTI common
+TARGET_COMMON_QTI_COMPONENTS := bt
 
 # Ramdisk
 PRODUCT_PACKAGES += \
